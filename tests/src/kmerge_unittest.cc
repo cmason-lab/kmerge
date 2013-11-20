022edfde7cc4251f22ed09d803d61cc2ddf8426a
Darryl Reeves <dar326@cornell.edu>
2013-11-19 20:17:03 -0500

    HDF5 to Sparse Matrix test implemented

--- tests/src/kmerge_unittest.cc
+++ tests/src/kmerge_unittest.cc
@@ -7,7 +7,9 @@
 #include "queryProcessor.h"
 #include "indexBuilder.h"
 #include "catch.hpp"
+#include "armadillo"
 
+using namespace arma;
 
 #ifndef H5_NO_NAMESPACE
 #ifndef H5_NO_STD
@@ -127,14 +129,14 @@ class HashTestFixture  {
 
 TEST_CASE_METHOD(HashTestFixture, "ReadHDF5", "[HashTest]") {
   const string kmer1("ACTGA");
-  const int kmer1_count = 4;
+  const uint kmer1_count = 4;
   const string kmer2("ATCGT");
-  const int kmer2_count = 3;
+  const uint kmer2_count = 3;
 
   uint kmer1_hash_val = hashKmer(kmer1);
   uint kmer2_hash_val = hashKmer(kmer2);
-  REQUIRE(612248635 == kmer1_hash_val);
-  REQUIRE(2960106173 == kmer2_hash_val);
+  REQUIRE((uint) 612248635 == kmer1_hash_val);
+  REQUIRE((uint) 2960106173 == kmer2_hash_val);
  
   const H5std_string FILE_NAME( "/home/darryl/Development/kmerge/tests/example.h5" );
   const H5std_string DATASET_NAME( "test_ds" );
@@ -163,6 +165,8 @@ TEST_CASE_METHOD(HashTestFixture, "ReadHDF5", "[HashTest]") {
   hsize_t dims[2];
   int ndims = in_filespace.getSimpleExtentDims( dims, NULL);
   
+  REQUIRE(ndims == 2);
+
   /*                                                                                                          
    * Define the memory space to read dataset.                                                                                                       
    */
@@ -171,7 +175,7 @@ TEST_CASE_METHOD(HashTestFixture, "ReadHDF5", "[HashTest]") {
   /*                                                                                                                                                
    * Read dataset back and display.                                                                                                                 
    */
-  int data_out[dims[0]][dims[1]];  // buffer for dataset to be read                                        
+  uint data_out[dims[0]][dims[1]];  // buffer for dataset to be read                                        
   in_dataset.read( data_out, PredType::NATIVE_UINT, mspace2, in_filespace );
 
   REQUIRE(kmer1_hash_val == data_out[0][0]);
@@ -200,9 +204,9 @@ TEST_CASE_METHOD(HashTestFixture, "HashKmerAndAppendToHDF5", "[HashTest]") {
   uint kmer_to_append_count = 2;
   vector[0][1] = kmer_to_append_count;
   const string kmer1("ACTGA");
-  const int kmer1_count = 4;
+  const uint kmer1_count = 4;
   const string kmer2("ATCGT");
-  const int kmer2_count = 3;
+  const uint kmer2_count = 3;
 
   uint kmer1_hash_val = hashKmer(kmer1);
   uint kmer2_hash_val = hashKmer(kmer2);
@@ -214,6 +218,8 @@ TEST_CASE_METHOD(HashTestFixture, "HashKmerAndAppendToHDF5", "[HashTest]") {
   DataSpace file_space = in_dataset.getSpace();
   int ndims = file_space.getSimpleExtentDims( dims1, NULL);
   int rank = file_space.getSimpleExtentNdims();
+
+  REQUIRE(ndims == 2);
 
   /*
    * Extend dataset to allow for new data.
@@ -261,7 +267,7 @@ TEST_CASE_METHOD(HashTestFixture, "HashKmerAndAppendToHDF5", "[HashTest]") {
   DataSpace mem_space2(rank, size);
 
 
-  int data_out[size[0]][size[1]];  // buffer for dataset to be read                                                                                                                                                                          
+  uint data_out[size[0]][size[1]];  // buffer for dataset to be read                     
   in_dataset2.read( data_out, PredType::NATIVE_UINT, mem_space2, file_space3 );
 
   REQUIRE(kmer1_hash_val == data_out[0][0]);
@@ -273,7 +279,7 @@ TEST_CASE_METHOD(HashTestFixture, "HashKmerAndAppendToHDF5", "[HashTest]") {
 
 }
 
-TEST_CASE_METHOD(HashTestFixture, "HashKmerAndFindVallue", "[HashTest]") {
+TEST_CASE_METHOD(HashTestFixture, "HashKmerAndFindValue", "[HashTest]") {
   const H5std_string FILE_NAME( "/home/darryl/Development/kmerge/tests/example.h5" );
   const H5std_string DATASET_NAME( "test_ds" );
   const string kmer("ATCGT");
@@ -363,8 +369,8 @@ TEST_CASE_METHOD(HashTestFixture, "HashKmerAndWriteToFixedHDF5", "[HDF5Test]") {
   const string kmer2("ATCGT");
   const int kmer2_count = 3;
 
-  uint kmer1_hash_val = hashKmer(kmer1);
-  uint kmer2_hash_val = hashKmer(kmer2);
+  uint kmer1_hash_val = 45232;//hashKmer(kmer1); /* Make value in unsigned short range */
+  uint kmer2_hash_val = 7532;//hashKmer(kmer2); /* Make value in unsigned short range */
 
   const H5std_string FILE_NAME( "/home/darryl/Development/kmerge/tests/fixed.h5" );
   const H5std_string DATASET_NAME( "org1" );
@@ -377,10 +383,9 @@ TEST_CASE_METHOD(HashTestFixture, "HashKmerAndWriteToFixedHDF5", "[HDF5Test]") {
    */
 
 
-  const uint FSPACE_DIM1 = (uint) pow(2,32)-1;
+  const uint FSPACE_DIM1 = USHRT_MAX;
   const int FSPACE_DIM2 = 1;
 
-  std::cout << (uint) pow(2,32)-1 << endl;
   /*
    * Create a file.
    */
@@ -408,12 +413,6 @@ TEST_CASE_METHOD(HashTestFixture, "HashKmerAndWriteToFixedHDF5", "[HDF5Test]") {
 
 
   DataSet* dataset = new DataSet(file->createDataSet(DATASET_NAME, PredType::NATIVE_UINT, fspace, plist));
-
-  /* 
-   * Create dataspace for the first dataset.
-   */
-
-  hsize_t dim1[] = {FSPACE_DIM1, FSPACE_DIM2};
 
 
   /*
@@ -445,4 +444,157 @@ TEST_CASE_METHOD(HashTestFixture, "HashKmerAndWriteToFixedHDF5", "[HDF5Test]") {
 
   delete dataset;
   delete file;
+}
+
+
+TEST_CASE("WriteSparseMatrixToHDF5AndReadFromAramdillo", "[SparseHDF5Test]") {
+  const string kmer1("ACTGA");
+  const uint kmer1_count = 4;
+  const string kmer2("ATCGT");
+  const uint kmer2_count = 3;
+
+  uint kmer1_hash_val = hashKmer(kmer1);
+  uint kmer2_hash_val = hashKmer(kmer2);
+
+  const H5std_string FILE_NAME( "/home/darryl/Development/kmerge/tests/sparse.h5" );
+  const H5std_string GROUP_NAME( "/org1" );
+  const H5std_string HASH_DATASET_NAME( "/org1/kmer_hash" );
+  const H5std_string COUNT_DATASET_NAME( "/org1/count" );
+  hsize_t maxdims = H5S_UNLIMITED;
+  hsize_t chunk_dims = 2;
+  const int FSPACE_RANK = 1;
+  const int FSPACE_DIM = 2;
+
+  /*
+   * Create a file and group;
+   */
+
+  H5File* file = new H5File( FILE_NAME, H5F_ACC_TRUNC );
+
+  Group* group = new Group( file->createGroup( "/org1" ));
+
+  /*                                                                                                                             
+   * Create property list for a dataset and set up fill values.                                                                           
+   */
+
+  uint fillvalue = 0;   /* Fill value for the dataset */
+  DSetCreatPropList plist;
+  plist.setChunk(1, &chunk_dims);
+  plist.setFillValue(PredType::NATIVE_UINT, &fillvalue);
+
+  /*                                                                                                 
+   * Create dataspace for the dataset in the file.  
+   */
+
+                                       
+  hsize_t fdim = FSPACE_DIM; // dim size of ds (on disk)
+  DataSpace fspace( FSPACE_RANK, &fdim, &maxdims);
+
+  /*                               
+   * Create datasets and write it into the file.           
+   */
+
+
+  DataSet* hash_dataset = new DataSet(file->createDataSet(HASH_DATASET_NAME, PredType::NATIVE_UINT, fspace, plist));
+  DataSet* count_dataset = new DataSet(file->createDataSet(COUNT_DATASET_NAME, PredType::NATIVE_UINT, fspace, plist));
+
+  /* 
+   * Create dataspace for the datasets.
+   */
+
+  hsize_t dim1 = FSPACE_DIM;
+
+  /* 
+   * Dimension size of the first dataset (in memory)  
+   */
+
+  DataSpace mspace1( FSPACE_RANK, &dim1 );
+  uint vector[FSPACE_DIM];     
+
+  // vector buffer for dset                                                                                                                                                                        
+  vector[0] = kmer1_hash_val;
+  vector[1] = kmer2_hash_val;
+
+  hash_dataset->write( vector, PredType::NATIVE_UINT, mspace1, fspace );
+
+  vector[0] = kmer1_count;
+  vector[1] = kmer2_count;
+
+  count_dataset->write( vector, PredType::NATIVE_UINT, mspace1, fspace );
+
+  /*  
+   * Close the dataset and the file.     
+   */
+
+
+  hash_dataset->close();
+  delete hash_dataset;
+  count_dataset->close();
+  delete count_dataset;
+  group->close();
+  delete group;
+  file->close();
+  delete file;
+
+  /*                                                                                                                                       
+   * Open the specified file and the specified dataset in the file.                                                                            
+   */
+  H5File in_file( FILE_NAME, H5F_ACC_RDONLY );
+  DataSet in_hash_dataset = in_file.openDataSet( HASH_DATASET_NAME );
+  DataSet in_count_dataset = in_file.openDataSet( COUNT_DATASET_NAME );
+  /*                                                                                                    
+   * Get dataspace of the dataset.
+   */
+  DataSpace filespace = in_hash_dataset.getSpace();
+
+  /*                                                                                                                   
+   * Get the number of dimensions in the dataspace. 
+   */
+  int hash_rank = filespace.getSimpleExtentNdims();
+
+  REQUIRE(hash_rank == 1);
+
+  /*                                                                                          
+   * Get the dimension size of each dimension in the dataspace and                                                                        
+   * display them.                                                                                                            
+   */
+  hsize_t dims;
+  int ndims = filespace.getSimpleExtentDims( &dims, NULL);
+  
+  REQUIRE(dims == (hsize_t) 2);
+  REQUIRE(ndims == 1);
+
+  /*                                                                                                          
+   * Define the memory space to read dataset.                                                                                                       
+   */
+  DataSpace mspace(hash_rank, &dims);
+
+  /*                                                                                                                                                
+   * Read dataset.                                                                                                                 
+   */
+  std::vector<uword> hashed_kmers(dims);
+  in_hash_dataset.read( &hashed_kmers[0], PredType::NATIVE_ULLONG, mspace, filespace );
+
+
+  REQUIRE(kmer1_hash_val == hashed_kmers[0]);
+  REQUIRE(kmer2_hash_val == hashed_kmers[1]);
+
+  uint *hash_counts = new uint[dims];
+  in_count_dataset.read( hash_counts, PredType::NATIVE_UINT, mspace, filespace );
+
+  REQUIRE(kmer1_count == hash_counts[0]);
+  REQUIRE(kmer2_count == hash_counts[1]);
+
+
+  uword temp[] = {0, dims}; // need to have 1 more entry than the number of columns which is greater than the length of "counts"
+  uvec org_start(temp, dims);
+
+  uvec hashes(hashed_kmers);
+  Col<uint> counts(hash_counts, dims);
+  SpMat<uint> A(hashes, org_start, counts, UINT_MAX, org_start.n_elem - 1);
+
+  REQUIRE(A(kmer1_hash_val, 0) == kmer1_count);
+  REQUIRE(A(kmer2_hash_val, 0) == kmer2_count);
+
+  delete [] hash_counts;
 }
