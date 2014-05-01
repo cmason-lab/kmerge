03078a8c5a3baaa84b6d4fccf5a2b6a532a85d9a
Darryl Reeves <dar326@cornell.edu>
2014-05-01 12:49:01 -0400

    Downlaoding genomes performs fewer calls to the NCBI servers

--- scripts/process_seqs.py
+++ scripts/process_seqs.py
@@ -36,14 +36,37 @@ def get_taxonomy(d, ncbi_pjid):
     tax_id = link_record[0]['LinkSetDb'][0]['Link'][0]['Id']
     handle = Entrez.efetch(db="taxonomy", id=tax_id)
     tax_record = Entrez.read(handle)
+    handle.close()
     tax_file = open("%s/taxonomy.txt" % d, 'w')
     for taxon in tax_record[0]['LineageEx']:
         if(taxon['Rank'] != 'no rank'):
             tax_file.write("%s\t%s\n" % (taxon['Rank'], taxon['ScientificName']))
 
-def find_refseq_records(ncbi_pjid_list):
-    handle = Entrez.elink(dbfrom="bioproject", db="bioproject", id=ncbi_pjid_list)
-    records = Entrez.read(handle)
+def convert_virus_accessions(ids, split_size=100):
+    # get virus NCBI project ids
+    virus_ncbi_pjids = set()
+    records = []
+    for split_ids in [ ids[i:i+split_size] for i in range(0, len(ids), split_size) ]:
+        handle = Entrez.elink(dbfrom="nuccore", db="bioproject", id=split_ids)
+        records.extend(Entrez.read(handle))
+        handle.close()
+    for record in records:
+        try:
+            ncbi_pjid = record['LinkSetDb'][0]['Link'][0]['Id']
+            virus_ncbi_pjids.add(ncbi_pjid)
+        except Exception as inst:
+            sys.stderr.write("!%s!\n" % record['IdList'][0])
+        
+    for ncbi_pjid in virus_ncbi_pjids:
+        sys.stdout.write("%s\n" % ncbi_pjid)
+                                                                                                                                                                                                                                                                
+            
+def find_refseq_records(ncbi_pjid_list, split_size=100):
+    records = []
+    for split_ids in [ ncbi_pjid_list[i:i+split_size] for i in range(0, len(ncbi_pjid_list), split_size) ]:
+        handle = Entrez.elink(dbfrom="bioproject", db="bioproject", id=split_ids)
+        records.extend(Entrez.read(handle))
+        handle.close()
     query_ids = set()
     skip_ids = set()
     # loop over each record of project ids and linked project ids
@@ -51,8 +74,13 @@ def find_refseq_records(ncbi_pjid_list):
         base_id = record['IdList'][0]
         if base_id in query_ids or base_id in skip_ids:
             continue
-        handle = Entrez.esummary(db="bioproject", id=base_id)
-        summary_record = Entrez.read(handle)
+        summary_record = None
+        try:
+            handle = Entrez.esummary(db="bioproject", id=base_id)
+            summary_record = Entrez.read(handle)
+            handle.close()
+        except:
+            sys.stderr.write("!%s!\n" % base_id)
         if summary_record['DocumentSummarySet']['DocumentSummary'][0]['Project_Data_Type'] == "RefSeq Genome":
             # add id to query set
             query_ids.add(base_id)
@@ -68,9 +96,14 @@ def find_refseq_records(ncbi_pjid_list):
             if skip_rest:
                 skip_ids.add(id)
                 continue
-            # get summary 
-            handle = Entrez.esummary(db="bioproject", id=id)
-            summary_record = Entrez.read(handle)
+            # get summary
+            summary_record = None
+            try:
+                handle = Entrez.esummary(db="bioproject", id=id)
+                summary_record = Entrez.read(handle)
+                handle.close()
+            except:
+                sys.stderr.write("!%s!\n" % id)
             if summary_record['DocumentSummarySet']['DocumentSummary'][0]['Project_Data_Type'] == "RefSeq Genome":
                 query_ids.add(id)
                 skip_rest = True
@@ -81,112 +114,76 @@ def find_refseq_records(ncbi_pjid_list):
     for query_id in query_ids:
         sys.stdout.write("%s\n" % query_id)
             
-#def process_gold_genomes(base, genomes, ncbi_pjid, seq_format, pat):
-def process_genomes(base, ncbi_pjid, seq_format, pat):
+def process_genomes(base, record, seq_format, pat):
+    ncbi_pjid = record['IdList'][0]
     d = base + ncbi_pjid
     os.mkdir(d)
     try:
-        #row = genomes[genomes['NCBI PROJECT ID'] == ncbi_pjid]
         # write out classifications to taxonomy.txt
         get_taxonomy(d, ncbi_pjid)
-        handle = Entrez.elink(dbfrom="bioproject", db="nuccore", id=ncbi_pjid)
-        record = Entrez.read(handle)
-        links = record[0]['LinkSetDb'][0]['Link']
-        records = []
-        for link in links:
-            ncid = link['Id']
-            handle = Entrez.efetch(db="nuccore", id=ncid, rettype=seq_format)
-            remove_ambiguous_bases(handle, "%s/%s.fasta.gz" % (d, ncbi_pjid), seq_format, pat)
-    except Exception as inst:
-        print "!%s!" % ncbi_pjid
-        print type(inst)
-        print inst.args
-        print inst
-
-def process_entrez_genomes(base, accession_id, seq_format, pat):
-    try:
-        handle = Entrez.esearch(db="nucleotide", term=accession_id)
-        search_record = Entrez.read(handle)
-        ncid = search_record['IdList'][0]
-        handle = Entrez.elink(dbfrom="nuccore", db="bioproject", id=ncid)
-        link_record = Entrez.read(handle)
-        ncbi_pjid = link_record[0]['LinkSetDb'][0]['Link'][0]['Id']
-        print "%s <-> %s" % (accession_id, ncbi_pjid)
-        d = base + ncbi_pjid
-        os.mkdir(d)
-        handle = Entrez.efetch(db="nuccore", id=ncid, rettype=seq_format)
+        
+        ncid_list = ",".join([ link['Id'] for link in record['LinkSetDb'][0]['Link'] ])
+        handle = Entrez.efetch(db="nuccore", id=ncid_list, rettype=seq_format)
         remove_ambiguous_bases(handle, "%s/%s.fasta.gz" % (d, ncbi_pjid), seq_format, pat)
-        get_taxonomy(d, ncbi_pjid)
+        handle.close()
     except Exception as inst:
-        print accession_id
-        print type(inst)
-        print inst.args
-        print inst
+        sys.stderr.write("!%s!\n" % ncbi_pjid)
+        sys.stderr.write("%s\n" % type(inst))
             
 def main():
-    parser = argparse.ArgumentParser(description='Download genomes')
+    parser = argparse.ArgumentParser(description='Process sequences for KMerge')
     parser.add_argument('mode', metavar='MODE', help='run mode', choices=['download_fasta', 'remove_N', 'virusTOprjna', 'prjnaTOrefseq'])
-    parser.add_argument('-i', '--in', metavar='INPUT', type=FileType(), default=sys.stdin, help='location of input file')
+    parser.add_argument('i', metavar='INPUT', type=FileType(), default=sys.stdin, help='location of input file')
     parser.add_argument('-d', '--dir', metavar='OUTPUT', default=os.getcwd(), help='location to write output files')
     parser.add_argument('-f', '--format', metavar='FORMAT', default='fastq', help='format of input file', choices=['fasta', 'fastq', 'fastq-solexa'])
     parser.add_argument('-t', '--num_threads', metavar='NUM THREADS', default=1, type=int, help='specify number of threads to use')
+    parser.add_argument('-g', '--no_gold', default=False, action='store_true', help='do not use GOLD input file ("gold.csv")')
     args = parser.parse_args()
 
     mode = args.mode
     pat = re.compile(r'N+', re.IGNORECASE)
     Entrez.email = 'dar326@cornell.edu'
-
+    split_size = 100
     
     if mode == 'virusTOprjna':
-        pass
+        ids = []
+        for line in args.i:
+            ids.append(line.strip())
+            
+        convert_virus_accessions(ids)
+    
     elif mode == 'prjnaTOrefseq':
         ids = []
-        for line in open("prjna_list.txt", "r"):
-            ids.append(line.strip())
+        if not args.no_gold:
+            df = pd.read_csv("gold.csv", dtype=object)
+            genomes = df[(df['PROJECT TYPE'] == 'Whole Genome Sequencing') & (df['PROJECT STATUS'] == 'Complete and Published') & (df['AVAILABILITY'] == 'Public') & (df['SEQUENCING STATUS'] == 'Complete')].dropna(subset=['NCBI PROJECT ID'])
+            ids.extend(list(genomes['NCBI PROJECT ID']))
+        if args.i:        
+            for line in args.i:
+                ids.append(line.strip())
 
         find_refseq_records(ids)
     elif mode == 'download_fasta':
-        Entrez.email = 'dar326@cornell.edu'
 
         base = args.dir + "/"
         seq_format = "fasta"
         
-        df = pd.read_csv("gold.csv", dtype=object)
-        genomes = df[(df['PROJECT TYPE'] == 'Whole Genome Sequencing') & (df['PROJECT STATUS'] == 'Complete and Published') & (df['AVAILABILITY'] == 'Public') & (df['SEQUENCING STATUS'] == 'Complete')].dropna(subset=['NCBI PROJECT ID'])
-
-        # get virus NCBI project ids (refactor as virusTOprjna
-        virus_ncbi_pjids = set()
-        for accession_id in [ line.strip() for line in open("entrez.dsdna.txt").readlines() + open("entrez.ssdna.txt").readlines()]:
-            try:
-                handle = Entrez.esearch(db="nucleotide", term=accession_id)
-                search_record = Entrez.read(handle)
-                ncid = search_record['IdList'][0]
-                handle = Entrez.elink(dbfrom="nuccore", db="bioproject", id=ncid)
-                link_record = Entrez.read(handle)
-                ncbi_pjid = link_record[0]['LinkSetDb'][0]['Link'][0]['Id']
-                virus_ncbi_pjids.add(ncbi_pjid)
-                print "!%s , %s!" % (accession_id, ncbi_pjid)
-            except Exception as inst:
-                print "!%s!" % accession_id
-                print type(inst)
-                print inst.args
-                print inst
 
         pool = threadpool.ThreadPool(args.num_threads)
         
         # process genomes
-        for ncbi_pjid in list(genomes['NCBI PROJECT ID']) + list(virus_ncbi_pjids):
-            #request = threadpool.WorkRequest(process_gold_genomes, args=[base, genomes, ncbi_pjid, seq_format, pat])
-            request = threadpool.WorkRequest(process_genomes, args=[base, ncbi_pjid, seq_format, pat])
-            pool.putRequest(request)
-            #process_gold_genomes(base, genomes, ncbi_pjid, seq_format, pat)
-
-        #process Entrez genomes
-        #for accession_id in [ line.strip() for line in open("entrez.dsdna.txt").readlines() + open("entrez.ssdna.txt").readlines()]:
-            #request = threadpool.WorkRequest(process_entrez_genomes, args=[base, accession_id, seq_format, pat])
-            #pool.putRequest(request)
-            #process_entrez_genomes(base, accession_id, seq_format, pat)
-
+        ids = []
+        for line in args.i:
+            ids.append(line.strip())
+        records = []
+        for split_ids in [ ids[i:i+split_size] for i in range(0, len(ids), split_size) ]:
+            handle = Entrez.elink(dbfrom="bioproject", db="nuccore", id=split_ids)
+            records.extend(Entrez.read(handle))
+            handle.close()
+        for record in records:
+            request = threadpool.WorkRequest(process_genomes, args=[base, record, seq_format, pat])
+            pool.putRequest(request) 
+            
         pool.wait()
         
     elif mode == 'remove_N':
