78076aabb3edfdb1a0480e211f10e2329e124c71
Darryl Reeves <dar326@cornell.edu>
2014-05-05 03:09:10 -0400

    Trying fasta collection from blast db

--- scripts/process_seqs.py
+++ scripts/process_seqs.py
@@ -12,7 +12,11 @@ import threadpool
 from bs4 import BeautifulSoup
 from ftplib import FTP
 from urlparse import urlparse
+import urllib2
 import gzip
+import time
+from subprocess32 import check_output, CalledProcessError
+import shutil
 
 class FileType(argparse.FileType):
     def __call__(self, string):
@@ -91,19 +95,16 @@ def remove_ambiguous_bases(file_handle, output_filename, seq_format, pat):
     out_file.close()
 
 def get_taxonomy(d, ncbi_pjid):
-    try:
-        handle = Entrez.elink(dbfrom="bioproject", db="taxonomy", id=ncbi_pjid)
-        link_record = Entrez.read(handle)
-        tax_id = link_record[0]['LinkSetDb'][0]['Link'][0]['Id']
-        handle = Entrez.efetch(db="taxonomy", id=tax_id)
-        tax_record = Entrez.read(handle)
-        handle.close()
-        tax_file = open("%s/taxonomy.txt" % d, 'w')
-        for taxon in tax_record[0]['LineageEx']:
-            if(taxon['Rank'] != 'no rank'):
-                tax_file.write("%s\t%s\n" % (taxon['Rank'], taxon['ScientificName']))
-    except Exception as inst:
-        sys.stderr.write("Error getting taxonomy information for %s\n" % ncbi_pjid)
+    handle = Entrez.elink(dbfrom="bioproject", db="taxonomy", id=ncbi_pjid)
+    link_record = Entrez.read(handle)
+    tax_id = link_record[0]['LinkSetDb'][0]['Link'][0]['Id']
+    handle = Entrez.efetch(db="taxonomy", id=tax_id)
+    tax_record = Entrez.read(handle)
+    handle.close()
+    tax_file = open("%s/taxonomy.txt" % d, 'w')
+    for taxon in tax_record[0]['LineageEx']:
+        if('Rank' in taxon and taxon['Rank'] != 'no rank'):
+            tax_file.write("%s\t%s\n" % (taxon['Rank'], taxon['ScientificName']))
 
 def convert_virus_accessions(ids, split_size=100):
     # get virus NCBI project ids
@@ -177,40 +178,94 @@ def find_refseq_records(ncbi_pjid_list, split_size=100):
     for query_id in query_ids:
         sys.stdout.write("%s\n" % query_id)
             
-def process_genomes(base, record, seq_format, pat):
+def process_genomes(base, record, seq_format, pat, db_dir, retry=0, max_retry=2):
     ncbi_pjid = record['IdList'][0]
     d = base + ncbi_pjid
-    os.mkdir(d)
+    os.makedirs(d)    
+    #if retry > max_retry:
+    #    sys.stderr.write("!%s!\n" % ncbi_pjid)
+    #    sys.stderr.write("<class 'urllib2.HTTPError'>\n")
+    #    return
+    #try:
+    #    os.makedirs(d)
+    #except OSError:
+    #    if not os.path.isdir(d):
+    #        raise
+    #try:
+    # write out classifications to taxonomy.txt
     try:
-        # write out classifications to taxonomy.txt
         get_taxonomy(d, ncbi_pjid)
-    
-        ncid_list = ",".join([ link['Id'] for link in record['LinkSetDb'][0]['Link'] ])
+    except Exception as inst:
+        sys.stderr.write("Error getting taxonomy information for %s\n" % ncbi_pjid)
+        sys.stderr.write("!%s!\n" % ncbi_pjid)
+        sys.stderr.write("%s\n" % type(inst))
+        # delet directory if can't get taxonomy
+        shutil.rmtree(d)
+        return
+    results = None
+    try:
         handle = Entrez.esearch(db="nucleotide", term='%s[BioProject] AND biomol_genomic[PROP] AND srcdb_refseq[PROP] NOT ALTERNATE_LOCUS[Keyword] NOT FIX_PATCH[Keyword] NOT NOVEL_PATCH[Keyword] NOT CONTIG[Title] NOT SCAFFOLD[Title]' % ncbi_pjid, usehistory='y')
         results = Entrez.read(handle)
-        webenv = results["WebEnv"]
-        query_key = results["QueryKey"]
-        count = int(results["Count"])
-        batch_size = 100
-        #gzf = gzip.GzipFile("%s/%s.fasta.gz" % (d, ncbi_pjid), 'wb')
-        for start in range(0,count,batch_size):
-            handle = Entrez.efetch(db="nuccore", retstart=start, retmax=batch_size, webenv=webenv, query_key=query_key, rettype=seq_format)
-            remove_ambiguous_bases(handle, "%s/%s.fasta.gz" % (d, ncbi_pjid), seq_format, pat)
-            #gzf.write(handle.read())
-            handle.close()
-        #gzf.close()
     except Exception as inst:
         sys.stderr.write("!%s!\n" % ncbi_pjid)
         sys.stderr.write("%s\n" % type(inst))
+    webenv = results["WebEnv"]
+    query_key = results["QueryKey"]
+    count = int(results["Count"])
+    batch_size = 100
+    fasta_handle = open("%s/sequence.fa" % d, 'w+')
+    seq_count = 0
+    for start in range(0,count,batch_size):
+        results = None
+        try:
+            handle = Entrez.efetch(db="nuccore", retstart=start, retmax=batch_size, webenv=webenv, query_key=query_key, rettype="acc")
+            results = handle.read()
+            handle.close()
+        except Exception as inst:
+            sys.stderr.write("!%s!\n" % ncbi_pjid)
+            sys.stderr.write("%s\n" % type(inst))
+        nul_f = open(os.devnull, 'w')
+        for acc in results.split("\n"):
+            try:
+                output = check_output(["blastdbcmd", "-db", "%s/refseq_genomic" % db_dir, "-dbtype", "nucl", "-entry", "%s" % acc, ],
+                                universal_newlines=True, stderr=nul_f)
+                fasta_handle.write(output)
+                seq_count = seq_count + 1
+            except Exception as inst:
+                pass
+        if seq_count == 0:
+            nul_f.close()
+            fasta_handle.close()
+            shutil.rmtree(d)
+            sys.stderr.write("!%s!\nUnable to get any sequences from db\n" % ncbi_pjid)
+            return
+        nul_f.close()
+        fasta_handle.seek(0)
+        remove_ambiguous_bases(fasta_handle, "%s/%s.fasta.gz" % (d, ncbi_pjid), seq_format, pat)
+        fasta_handle.close()
+        os.remove("%s/sequence.fa" % d)
+#    except urllib2.HTTPError:
+        # if we have a HTTPError, retry
+#        fasta_file = "%s/%s.fasta.gz" % (d, ncbi_pjid)
+#        if os.path.isfile(fasta_file):
+#            os.remove(fasta_file)
+#        time.sleep(2) # sleep for 2 seconds to let server clear up any issues
+#        sys.stderr.write("Retrying %s\n" % ncbi_pjid)    
+#        process_genomes(base, record, seq_format, pat, db_dir, retry+1, max_retry)
+#    except Exception as inst:
+#        sys.stderr.write("!%s!\n" % ncbi_pjid)
+#        sys.stderr.write("%s\n" % type(inst))
             
 def main():
     parser = argparse.ArgumentParser(description='Process sequences for KMerge')
     parser.add_argument('mode', metavar='MODE', help='run mode', choices=['download_fasta', 'remove_N', 'virusTOprjna', 'prjnaTOrefseq'])
     parser.add_argument('i', metavar='INPUT', type=FileType(), default=sys.stdin, help='location of input file')
     parser.add_argument('-d', '--dir', metavar='OUTPUT', default=os.getcwd(), help='location to write output files')
+    parser.add_argument('-b', '--db_dir', metavar='DB_DIR', default=os.getcwd(), help='location of refseq genomic database')
     parser.add_argument('-f', '--format', metavar='FORMAT', default='fastq', help='format of input file', choices=['fasta', 'fastq', 'fastq-solexa'])
     parser.add_argument('-t', '--num_threads', metavar='NUM THREADS', default=1, type=int, help='specify number of threads to use')
     parser.add_argument('-g', '--no_gold', default=False, action='store_true', help='do not use GOLD input file ("gold.csv")')
+    parser.add_argument('-r', '--max_retry', metavar='MAX_RETRY', default=2, help='max number of times to retry genome download on http error') 
     args = parser.parse_args()
 
     mode = args.mode
@@ -243,7 +298,7 @@ def main():
             asm_ids = []
             for start in range(0,count,batch_size):
                 fetch_handle = Entrez.esummary(db="assembly", retstart=start, retmax=batch_size, webenv=nv_webenv, query_key=nv_query_key)
-                nv_summaries = Entrez.read(fetch_handle)
+                nv_summaries = Entrez.read(fetch_handle)                    
                 nv_pjids.extend([ summary['RS_BioProjects'][0]['BioprojectId'] for summary in nv_summaries['DocumentSummarySet']['DocumentSummary']])
         except Exception as inst:
             sys.stderr.write("Error converting non-virus assembly ids to bioproject ids\n")
@@ -262,7 +317,10 @@ def main():
             v_summaries.extend(Entrez.read(fetch_handle))
 
         v_pjids = [ summary['ProjectID'] for summary in v_summaries]
-        pjids = nv_pjids + v_pjids
+
+        
+        pjids = list(set(nv_pjids[:3] + v_pjids[:3]))
+
         records = []
         for split_ids in [ pjids[i:i+split_size] for i in range(0, len(pjids), split_size) ]:
             try:
@@ -274,7 +332,7 @@ def main():
                 sys.exit()
 
         for record in records:
-            request = threadpool.WorkRequest(process_genomes, args=[base, record, seq_format, pat])
+            request = threadpool.WorkRequest(process_genomes, args=[base, record, seq_format, pat, args.db_dir, 0, args.max_retry])
             pool.putRequest(request) 
             
         pool.wait()
